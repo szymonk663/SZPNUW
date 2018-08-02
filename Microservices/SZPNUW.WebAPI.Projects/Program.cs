5700b44f387658368c03e87c4036354ace112625
skrawieczek <szymonk663@gmail.com>
2018-08-02 11:53:37 +0200

    Zmiana portów na których wystawione są serwisy

--- Microservices/SZPNUW.WebAPI.Projects/Program.cs
+++ Microservices/SZPNUW.WebAPI.Projects/Program.cs
@@ -20,6 +20,7 @@ namespace SZPNUW.WebAPI.Projects
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:7005/")
                 .Build();
     }
 }
--- Microservices/SZPNUW.WebAPI.Subject/Program.cs
+++ Microservices/SZPNUW.WebAPI.Subject/Program.cs
@@ -20,6 +20,7 @@ namespace SZPNUW.WebAPI.Subject
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:7003/")
                 .Build();
     }
 }
